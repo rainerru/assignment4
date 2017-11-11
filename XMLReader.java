141af79a4946d4f639523b576aa4ac17e8d53ee3
rainerru <rainerru@stud.sbg.ac.at>
2017-11-11 14:29:32 +0100

    updating XMLReader

--- XMLReader.java
+++ XMLReader.java
@@ -1,30 +1,75 @@
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom;
+import org.w3c.dom.*;
+import java.io.File;
 
 public class XMLReader
 {
 
-	public void bla ()
+	public static void main (String[] args)
+	{
+		System.out.println("asdf");
+		try {
+		ItemList list0 = getListFromXMLFile("./input.xml");
+		System.out.println("list0.getprice (155.0) = " + list0.getPrice());
+		} catch ( Exception e ) { e.printStackTrace(); }
+	}
+
+	public static ItemList getListFromXMLFile ( String filename ) throws Exception
 	{ 
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(new File( filename ));
+			Element root = doc.getDocumentElement();
 
-		//some basic code to read an xml file. TODO: hardcoded source (filename)
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(new File("./input.xml"));
-		Element root = doc.getDocumentElement();
+			ItemList fullList = new ItemList("fullList");
+			
+			addListToList(root,fullList);
 
-		//some dummy code...... TODO: find a good way to access the elements and their attributes
-		NodeList nl = root.getChildNodes();
-		for (int i=0;i<nl.getLength();i++){
-			Node n = nl.item(i);
-			int type = n.getNodeType();
+			return fullList;
+	}
+
+	private static void addListToList ( Element e, ItemList list )
+	{
+		NodeList listOfNodes = e.getChildNodes();
+
+		for (int i=0;i<listOfNodes.getLength();i++){
+
+			Node currentNode = listOfNodes.item(i);
+			int type = currentNode.getNodeType();
+
 			if (type == Node.ELEMENT_NODE){
-				Element e = (Element) n;
-				System.out.println(e.getTagName() + ": " + e.getAttribute("name"));
+
+				Element currentElement = (Element) currentNode;
+				if ( currentElement.getTagName().equals("list") )
+					addListToList ( currentElement, list );
+				else 
+					addElementToList ( currentElement, list );
+
 			}
 		}
+	} 
 
-	}
+	private static void addElementToList ( Element currentElement, ItemList list )
+	{
+		if ( currentElement.getTagName().equals("book") )
+		{
+			Item newItem = new Book(currentElement.getAttribute("name"),
+				Double.parseDouble(currentElement.getAttribute("price")),
+				Integer.parseInt(currentElement.getAttribute("isbn")));
+			list.add(newItem);
+		} else
+		if ( currentElement.getTagName().equals("cd") )
+		{
+			Item newItem = new Cd(currentElement.getAttribute("name"),
+				Double.parseDouble(currentElement.getAttribute("price")));
+			list.add(newItem);
+		} else
+		{
+			Item newItem = new Item(currentElement.getAttribute("name"),
+				Double.parseDouble(currentElement.getAttribute("price")));
+			list.add(newItem);
+		} 
+	} 
 
 }
